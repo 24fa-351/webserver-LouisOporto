870ccbb83031c5bfedfa9013e5bfd22482a0f5c0
Louis Oporto <louisoporto042@csu.fullerton.edu>
2024-11-10 11:07:00 -0800

    Initialize http_message header

--- http_message.h
+++ http_message.h
@@ -2,11 +2,13 @@
 #define HTTP_MESSAGE_H
 
 typedef struct msg {
+  char* method;
+  char* path;
   char* http_version;
   char* body;
   int body_length;
-  char *headers;
-} https_client_message_t;
+  char* headers;
+} http_client_message_t;
 
 typedef enum {
   BAD_REQUEST,
@@ -14,8 +16,9 @@ typedef enum {
   MESSAGE
 } http_read_reuslt_t;
 
-void read_http_message(int socket_fd, https_client_message_t** http_msg, http_read_reuslt_t* result);
+// reponse: a message
+void http_client_message(int socket_fd, http_client_message_t** http_msg, http_read_reuslt_t* result);
 
-void http_client_message_free(https_client_message_t* http_msg);
+void http_client_message_free(http_client_message_t* http_msg);
 
 #endif
