c0e52fcb9ba1a0c485a08b3fe5a9ad8a8f9116f2
Louis Oporto <louisoporto042@csu.fullerton.edu>
2024-11-10 10:11:38 -0800

    Created handling connection funnction

--- webserver.c
+++ webserver.c
@@ -5,43 +5,33 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
+#include "http_message.h"
+
+#define PORT 25565
+#define LISTEN_BACKLOG 5
+
+int respond_to_http_client_message(int socket_fd, https_client_message_t* http_msg) {
+  char* response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+  write(socket_fd, response, strlen(response));
+  return 0;
+}
+
+// innput pointer is freed by this funtion
+void handleConnection(int* sock_fd_ptr) {   
+    int sock_fd = *sock_fd_ptr;
+    free(sock_fd_ptr);
+
+    printf("Handling connection %d\n", sock_fd);
+    char buffer[1024];
+    int bytes_read = read(sock_fd, buffer, sizeof(buffer));
+    printf("Received: %s\n", buffer);
+    write(sock_fd, buffer, bytes_read);
+
+    printf("Done with handling connection %d\n", sock_fd);
+}
 int main(int argc, char* argv[]) {
-    int sockfd, newsockfd, portno, clilen;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
-    if (argc < 2) {
-        fprintf(stderr, "ERROR, no port provided\n");
-        exit(1);
-    }
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        error("ERROR opening socket");
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        error("ERROR on binding");
-    }
-    listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    if (newsockfd < 0) {
-        error("ERROR on accept");
-    }
-    bzero(buffer, 256);
-    n = read(newsockfd, buffer, 255);
-    if (n < 0) {
-        error("ERROR reading from socket");
-    }
-    printf("Here is the message: %s\n", buffer);
-    n = write(newsockfd, "I got your message", 18);
-    if (n < 0) {
-        error("ERROR writing to socket");
-    }
-    return 0;
+    
 }
